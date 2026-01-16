d840c93dd1889e712cf6daf67a6c43e52ab82cb8
Jonathan Sedrez <jonathansedrez@gmail.com>
2026-01-15 23:26:53 -0300

    feat: add million starter

--- react-million/vite.config.ts
+++ react-million/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import million from "million/compiler";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [million.vite({}), react()],
+});
