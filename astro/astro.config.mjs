6bfedf2f7e26c999f81025d4e81a4bb5da5142cf
Jonathan Sedrez <jonathansedrez@gmail.com>
2026-02-09 21:31:01 -0300

    feat(astro): add react

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   image: {
     domains: ["mir-s3-cdn-cf.behance.net"],
   },
+
+  integrations: [react()],
 });
